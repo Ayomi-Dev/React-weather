ff47bf92f23661846ff3ddc6ea4a7e8c43d5f28e
Ayomide <ayomideolakunlea1@gmail.com>
2023-05-31 13:05:30 +0100

    removed unused component

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import {Link} from "react-router-dom"
-import Counter from "./Counter";
 
 const Search = ({onCityChange}) => {
     const [cityName, setCityName] = useState('')
